865971962826fd45580673406176a189efcfd539
Linh Duy <test@MacBook-Pro.local>
2024-05-17 11:40:25 +0700

    update loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Row from "./components/Row";
+import { Spin, message } from "antd";
 
 interface PostData {
   userId: number;
@@ -10,16 +11,27 @@ interface PostData {
 }
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(true);
   const [posts, setPosts] = useState<PostData[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-        const data = await res.json();
-        setPosts(data);
+        setTimeout(async () => {
+          const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+          if (!res.ok) {
+            throw new Error("Không thể gọi dữ liệu");
+          }
+          const data = await res.json();
+          setPosts(data);
+          setLoading(false);
+          message.success("Hiển thị dữ liệu thành công");
+        }, 1000);
       } catch (err) {
         console.log("err", err);
+        setLoading(false);
+        message.error("Không thể hiển thị dữ liệu!");
       }
     };
     fetchData();
@@ -28,19 +40,21 @@ function App() {
   return (
     <>
       <div className="table-wrapper">
-        <table className="fl-table">
-          <thead>
-            <tr>
-              <th>UserId</th>
-              <th>Id</th>
-              <th>Title</th>
-              <th>Body</th>
-            </tr>
-          </thead>
-          {posts.map((e) => (
-            <Row key={e.id} {...e} />
-          ))}
-        </table>
+        <Spin spinning={loading}>
+          <table className="fl-table">
+            <thead>
+              <tr>
+                <th>UserId</th>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Body</th>
+              </tr>
+            </thead>
+            {posts.map((e) => (
+              <Row key={e.id} {...e} />
+            ))}
+          </table>
+        </Spin>
       </div>
     </>
   );
